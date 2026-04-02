bc88c1e68f6be3bcecc26663cf0dd939f3363d1b
Vibhav Mahendra Bilgoji <vibhavbilgoji@gmail.com>
2026-04-02 20:13:24 +0530

    Completed Employee Roles

---
+++ Expt 7/Employee Roles/EmpRoles.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class EmpRoles{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("Select the role of the employee:");
+            System.out.println("1. Employee");
+            System.out.println("2. Manager");
+            System.out.println("3. Developer");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            Employee emp;
+            switch(choice){
+                case 1:
+                    emp = new Employee();
+                    emp.work();
+                    break;
+                case 2:
+                    emp = new Manager();
+                    emp.work();
+                    break;
+                case 3:
+                    emp = new Developer();
+                    emp.work();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice! Please try again.\n");
+        }
+    }
+}
+
+class Employee{
+    public void work(){
+        System.out.println("Employee is working...\n");
+    }
+}
+
+class Manager extends Employee{
+    @Override
+    public void work(){
+        System.out.println("Manager is managing the team...\n");
+    }
+}
+
+class Developer extends Employee{
+    @Override
+    public void work(){
+        System.out.println("Developer is writing code...\n");
+    }
+}
